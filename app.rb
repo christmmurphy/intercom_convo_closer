06dbd13fe0dfd67755ad714858968d6ae02b0f74
christmmurphy <chris.murphy@intercom.io>
2018-07-03 17:52:27 -0500

    added simple sinatra ui

--- app.rb
+++ app.rb
@@ -1,47 +1,37 @@
 require 'Intercom'
+require 'rubygems'
+require 'sinatra'
 
-# Take Input
-puts "Enter the id of the inbox you would like to close:"
-admin_id = gets.chomp
-admin_id.to_i
-puts "Thanks!"
-puts "Enter PAT:"
-pat = gets.chomp
-pat = pat.to_s
-
-# Auth Section For Client
-intercom = Intercom::Client.new(token: pat);
-
-
-# Choose Admin:
-convos = intercom.conversations.find_all(:type => 'admin', :id => admin_id, :open => true)
-
-puts "============================="
-
-  convo_ids = []
-
-  convos.each do |convo|
-    convo_ids.push(convo.id)
+  get '/' do
+    erb :index
   end
 
-  puts convo_ids
+  post '/close' do
+    pat = params[:pat]
+    inbox_id = params[:inbox_id]
+    admin_id = params[:admin_id]
 
-puts "============================="
-
-puts "CLOSING"
-
-  convo_ids.each do |convo|
-
-    intercom.conversations.close(id: convo, admin_id: admin_id)
-    puts "closing convo: #{convo}"
-
-    # Handle Rate Limits
-    if not intercom.rate_limit_details[:remaining].nil? and intercom.rate_limit_details[:remaining] < 2
-      sleep_time = intercom.rate_limit_details[:reset_at].to_i - Time.now.to_i
-      puts("Waiting for #{sleep_time} seconds to allow for rate limit to be reset")
-      sleep sleep_time
+    # Auth Section For Client
+    intercom = Intercom::Client.new(token: pat);
+    # Choose Admin:
+    convos = intercom.conversations.find_all(:type => 'admin', :id => 273930, :open => false)
+    convo_ids = []
+      convos.each do |convo|
+        convo_ids.push(convo.id)
+      end
+    puts convo_ids
+    puts "============================="
+    puts "CLOSING..."
+      convo_ids.each do |convo|
+        intercom.conversations.open(id: convo, admin_id: 273930)
+        puts "closing convo: #{convo}"
+        # Handle Rate Limits
+        if not intercom.rate_limit_details[:remaining].nil? and intercom.rate_limit_details[:remaining] < 2
+          sleep_time = intercom.rate_limit_details[:reset_at].to_i - Time.now.to_i
+          puts("Waiting for #{sleep_time} seconds to allow for rate limit to be reset")
+          sleep sleep_time
+      end
     end
-
+    puts "COMPLETE"
+    erb :index
   end
-
-puts "COMPLETE"
