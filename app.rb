b1a80a29db3eb528f7322ebd9d927ad1d2a1aa2c
christmmurphy <chris.murphy@intercom.io>
2018-07-03 16:38:28 -0500

    MVP, script that takes input and closes each convo

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'Intercom'
+
+# Take Input
+puts "Enter the id of the inbox you would like to close:"
+admin_id = gets.chomp
+admin_id.to_i
+puts "Thanks!"
+puts "Enter PAT:"
+pat = gets.chomp
+pat = pat.to_s
+
+# Auth Section For Client
+intercom = Intercom::Client.new(token: pat);
+
+
+# Choose Admin:
+convos = intercom.conversations.find_all(:type => 'admin', :id => admin_id, :open => true)
+
+puts "============================="
+
+  convo_ids = []
+
+  convos.each do |convo|
+    convo_ids.push(convo.id)
+  end
+
+  puts convo_ids
+
+puts "============================="
+
+puts "CLOSING"
+
+  convo_ids.each do |convo|
+
+    intercom.conversations.close(id: convo, admin_id: admin_id)
+    puts "closing convo: #{convo}"
+
+    # Handle Rate Limits
+    if not intercom.rate_limit_details[:remaining].nil? and intercom.rate_limit_details[:remaining] < 2
+      sleep_time = intercom.rate_limit_details[:reset_at].to_i - Time.now.to_i
+      puts("Waiting for #{sleep_time} seconds to allow for rate limit to be reset")
+      sleep sleep_time
+    end
+
+  end
+
+puts "COMPLETE"
