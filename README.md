179a8fa0faa60d5ea3c84881a60230305eaee79a
christmmurphy <chris.murphy@intercom.io>
2018-07-09 17:53:26 -0500

    I added a Sinatra UI and added some logic to open or close

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # intercom_convo_closer
-close the conversations in a specific inbox in Intercom
+Target an Intercom inbox to close or open all conversations in it.
+
+![Alt Text](https://downloads.intercomcdn.com/i/o/66974103/7bbb6deb0d5076d295774823/Screen+Recording+2018-07-09+at+05.45+PM.gif)
--- app.rb
+++ app.rb
@@ -6,32 +6,68 @@ require 'sinatra'
     erb :index
   end
 
-  post '/close' do
+  post '/inbox' do
+    operation = params[:operation]
     pat = params[:pat]
     inbox_id = params[:inbox_id]
-    admin_id = params[:admin_id]
+    @admin_id = params[:admin_id]
 
-    # Auth Section For Client
-    intercom = Intercom::Client.new(token: pat);
-    # Choose Admin:
-    convos = intercom.conversations.find_all(:type => 'admin', :id => 273930, :open => false)
-    convo_ids = []
-      convos.each do |convo|
-        convo_ids.push(convo.id)
-      end
-    puts convo_ids
-    puts "============================="
-    puts "CLOSING..."
-      convo_ids.each do |convo|
-        intercom.conversations.open(id: convo, admin_id: 273930)
-        puts "closing convo: #{convo}"
-        # Handle Rate Limits
-        if not intercom.rate_limit_details[:remaining].nil? and intercom.rate_limit_details[:remaining] < 2
-          sleep_time = intercom.rate_limit_details[:reset_at].to_i - Time.now.to_i
-          puts("Waiting for #{sleep_time} seconds to allow for rate limit to be reset")
-          sleep sleep_time
-      end
+    # Set up Intercom Ruby SDK
+    @intercom = Intercom::Client.new(token: pat);
+
+    # Grab the list of conversation id values based on the Open / Close toggle
+    if operation == "true"
+      puts "The value passed was: #{operation} | The user wants to OPEN this inbox. Now grabbing all the CLOSED conversation"
+      convos = @intercom.conversations.find_all(:type => 'admin', :id => @admin_id, :open => false)
+    else
+      puts "The value passed was: #{operation} | The user wants to CLOSE this inbox. Now grabbing all the OPEN conversation"
+      convos = @intercom.conversations.find_all(:type => 'admin', :id => @admin_id, :open => true)
     end
+
+    # Put those values into an array
+    @convo_ids = []
+      convos.each do |convo|
+        @convo_ids.push(convo.id)
+      end
+    puts "Grabbed #{@convo_ids.count} conversations"
+    puts "============================="
+
+    # Run the selected function
+    if operation == "true"
+      open_inbox
+    end
+
+    if operation == "false"
+      close_inbox
+    end
+
+    # Return to the home page
     puts "COMPLETE"
     erb :index
+  end
+
+  # FUNCTIONS USED BY THE APP
+  def check_rate
+    if not @intercom.rate_limit_details[:remaining].nil? and @intercom.rate_limit_details[:remaining] < 2
+        sleep_time = @intercom.rate_limit_details[:reset_at].to_i - Time.now.to_i
+        puts("Waiting for #{sleep_time} seconds to allow for rate limit to be reset")
+        sleep sleep_time
+      end
+    else
+  end
+
+  def open_inbox
+    @convo_ids.each do |convo|
+      @intercom.conversations.open(id: convo, admin_id: @admin_id)
+      puts "opening convo: #{convo}"
+      check_rate()
+    end
+  end
+
+  def close_inbox
+    @convo_ids.each do |convo|
+      @intercom.conversations.close(id: convo, admin_id: @admin_id)
+      puts "closing convo: #{convo}"
+      check_rate()
+    end
   end
